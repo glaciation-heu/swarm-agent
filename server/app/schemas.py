df42fec11cd8c484dcdf125012f80c2c1ca26948
Péter Forgács <38781608+fpetke@users.noreply.github.com>
2024-10-02 15:57:04 +0200

    resolve formatting issue

--- server/app/schemas.py
+++ server/app/schemas.py
@@ -11,7 +11,9 @@ class ResponseHead(BaseModel):
 class ResponseResults(BaseModel):
     bindings: list[Dict[str, Any]]
 
+
 # TODO make sure that the response is same as in Metadata Service
+
 
 class Message(BaseModel):
     message_type: str = "forward"
