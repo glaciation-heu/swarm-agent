375bde68b4901818d5882576ab35e2cb2b8611b2
Oleksandr Chepizhko <chepizhko@lakeside-labs.com>
2025-02-20 11:16:37 +0100

    bug fix

--- server/app/create_neighbors.py
+++ server/app/create_neighbors.py
@@ -118,7 +118,7 @@ def create_neighbors():
                 "\n\t\t" if len(triples) > 0 else ""
             ) + f"<{node}> <swarm:isNeighborOf> <{neighbor}> ."
 
-    triples += "\n\t\t" + f"{swarm_pods[1]} <swarm:hasKnowledgeOf> <Car1> ."
+    triples += "\n\t\t" + f"<{swarm_pods[1]}> <swarm:hasKnowledgeOf> <Car1> ."
     triples += "\n\t\t" + "<Car1> <swarm:hasColor> <Blue> ."
 
     query = f"""INSERT DATA {{
